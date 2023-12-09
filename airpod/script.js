0f988effcce133bbf967bdd24fc0c3afff9ab59f
sonht1109 <yourgitemail@example.com>
2023-12-10 00:15:53 +0700

    finish: airpod animation

--- airpod/script.js
+++ airpod/script.js
@@ -1,6 +1,10 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
 gsap.registerPlugin(ScrollTrigger);
 
 const FRAME_COUNT = 65;
@@ -9,9 +13,9 @@ const obj = {
 };
 
 const canvas = document.getElementById("hero-lightpass");
-const image = document.getElementById("ex");
-
-const h1 = document.querySelector(".hero-lookup h1");
+const scrollContainer = document.querySelector(".sticky-container");
+const heroLookup = document.querySelector(".hero-lookup");
+const heroContents = document.querySelectorAll(".hero-content");
 
 /**
  * @type CanvasRenderingContext2D
@@ -29,13 +33,33 @@ const images = Array.from({ length: FRAME_COUNT }).map((_, i) => {
 const render = () => {
   const img = images[obj.frame];
   const ratio = img.naturalWidth / img.naturalHeight;
-  canvas.width = window.innerWidth;
+  canvas.width = Math.max(window.innerWidth, 734);
   canvas.height = canvas.width / ratio;
   ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
   ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 };
 
-images[0].onload = render;
+images[0].onload = () => {
+  render();
+  gsap
+    .timeline()
+    .fromTo(
+      canvas,
+      { opacity: 0, scale: 0.7 },
+      { opacity: 1, duration: 0.2, delay: 0.4 }
+    )
+    .to(
+      canvas,
+      { opacity: 1, scale: 1, duration: 1, ease: "power1.out" },
+      "+=0.2"
+    )
+    .fromTo(
+      heroLookup,
+      { opacity: 0, scale: 0.8 },
+      { opacity: 1, scale: 1, duration: 1 },
+      "-=100%"
+    );
+};
 
 window.onresize = () => {
   render();
@@ -46,7 +70,53 @@ gsap.to(obj, {
   snap: "frame",
   ease: "none",
   scrollTrigger: {
-    scrub: 1,
+    scrub: true,
   },
   onUpdate: render,
+});
+
+const tlCanvas = gsap.timeline({
+  scrollTrigger: {
+    scrub: true,
+    trigger: scrollContainer,
+  },
+});
+
+tlCanvas.to(canvas, {
+  scale: 1.2,
+  duration: 1,
+});
+
+const tlHeroContent = gsap
+  .timeline({
+    scrollTrigger: {
+      scrub: true,
+      trigger: scrollContainer,
+    },
+  })
+  .to(
+    heroLookup,
+    {
+      scale: 1.1,
+      duration: 1,
+    },
+    0
+  )
+  .to(
+    heroLookup,
+    {
+      scale: 1.2,
+      opacity: 0,
+      duration: 0.4,
+    },
+    ">"
+  );
+
+heroContents.forEach((target, i) => {
+  tlHeroContent.fromTo(
+    target,
+    { opacity: 0, scale: 1 },
+    { opacity: 1, duration: 0.4, scale: 1.2 },
+    "+=1"
+  );
 });
